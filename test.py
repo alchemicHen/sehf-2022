55d3bdbac4d1a40bbea965e7ef1303bc207157a0
Adit Pareek <aditepic10@gmail.com>
2022-02-26 19:26:26 -0600

    Got data. Need to add both to df'

--- test.py
+++ test.py
@@ -1,7 +1,20 @@
 # Import package
 import pandas as pd
 import numpy as np
+import datetime as datetime
 
 # import from CSV
-prices = pd.read_csv('price_mid_wkly.csv', index_col = 0, low_memory = False)
+prices_df = pd.read_excel('USO_Historical_Data_30_Minute.xlsx', sheet_name = "Jan 2012", index_col=False)
+# print(prices_df.info())
+
+date_df = prices_df[prices_df['Close'].isna()]
+
+open_df = prices_df[prices_df['Time Interval'] == "09:30 - 10:00"]
+close_df = prices_df[prices_df['Time Interval'] == "15:30 - 16:00"]
+
+close_df['date'] = date_df['Time Interval']
+open_df['date'] = date_df['Time Interval']
+
+openclose_df = open_df.merge(close_df, on='date', how='left')
+print(openclose_df.tail())
 
